ee53e722bfff3c6d3a83bbd46b00c22a320b5cb4
Alex Merryman <16581372+alexmerryman@users.noreply.github.com>
2024-08-27 16:34:46 -0600

    myco-2: inat_obs_vouchers.py initial commit

---
+++ inat_obs_vouchers.py
@@ -0,0 +1,16 @@
+import reportlab
+
+
+def main():
+    ...
+    # get all obs > input ID number
+    # filter by kingdom/taxon (ex: fungi)
+    # get recent inat observation info (ID, location, datetime, etc)
+    # if obscured, leave location/datetime blank
+    # include species? or leave blank to be filled in?
+    # format as PDF
+    # download/save to print
+
+
+if __name__ == '__main__':
+    main()
